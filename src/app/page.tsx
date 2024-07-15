417fe7a4e40d491232eac7c5601e24d2436de395
Pramodya Sahan Dilushana <sahandil1013@gmail.com>
2024-07-15 16:51:55 +0530

    feat: define schema and table for files with a name field

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,12 +2,13 @@
 
 import {Button} from "@/components/ui/button";
 import {SignedIn, SignedOut, SignInButton, SignOutButton} from "@clerk/nextjs";
-import {useMutation} from "convex/react";
+import {useMutation, useQuery} from "convex/react";
 import {api} from "../../convex/_generated/api";
 
 
 export default function Home() {
     const createFile = useMutation(api.files.createFile)
+    const files = useQuery(api.files.getFiles)
 
     return (
         <main className="flex min-h-screen flex-col items-center justify-between p-24">
@@ -21,6 +22,9 @@ export default function Home() {
                     <Button>Sign In</Button>
                 </SignInButton>
             </SignedOut>
+            {files?.map(file => (
+                <div key={file._id}>{file.name}</div>
+            ))}
             <Button onClick={() => {
                 createFile({
                     name: "Hello World"
