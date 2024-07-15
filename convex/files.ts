52297cfa2ad838f1c904a27e2c8f5e7f03462ac0
Pramodya Sahan Dilushana <sahandil1013@gmail.com>
2024-07-15 16:57:14 +0530

    fix: ensure user is authenticated before allowing file upload

--- convex/files.ts
+++ convex/files.ts
@@ -1,11 +1,13 @@
 import {mutation, query} from "./_generated/server";
-import {v} from "convex/values";
+import {ConvexError, v} from "convex/values";
 
 export const createFile = mutation({
     args: {
         name: v.string(),
     },
     async handler(ctx, args) {
+        const identity = await ctx.auth.getUserIdentity()
+        if (!identity) throw new ConvexError("You must be logged in to upload a file")
         await ctx.db.insert("files", {
             name: args.name,
         });
@@ -15,6 +17,10 @@ export const createFile = mutation({
 export const getFiles = query({
     args: {},
     async handler(ctx, args) {
+        const identity = await ctx.auth.getUserIdentity()
+        if (!identity) {
+            return [];
+        }
         return ctx.db.query("files").collect()
     }
 })
